1cff7ee9b2f861bc34b7c1721c1f6b84e0a5c7c4
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-09-08 21:06:05 -0700

    Revert "intial commits with multithreading"
    This reverts commit cc084de3dc334fbfaaa6aeb98fb86f1b44fc8514.

--- main.py
+++ main.py
@@ -17,35 +17,33 @@ np.random.seed(seed=0)
 #               group_size_dist='uniform',
 #               placement_dist='colocate')
 
-cloud = Cloud(num_leafs=1056,
-              num_hosts_per_leaf=48,
-              max_vms_per_host=20,
-              num_tenants=3000,
-              min_vms_per_tenant=10,
-              max_vms_per_tenant=5000,
-              vm_dist='expon',
-              num_groups=100000,
-              min_group_size=5,
-              group_size_dist='uniform',
-              placement_dist='uniform',
-              num_bitmaps=32,
-              generate_bitmaps=True)
-
 # cloud = Cloud(num_leafs=48,
 #               num_hosts_per_leaf=48,
 #               max_vms_per_host=20,
 #               num_tenants=100,
 #               min_vms_per_tenant=10,
 #               max_vms_per_tenant=100,
-#               vm_dist='expon',
+#               vm_dist='expon-mean',
 #               num_groups=1000,
 #               min_group_size=5,
 #               group_size_dist='uniform',
-#               placement_dist='uniform',
-#               num_bitmaps=10,
-#               generate_bitmaps=True)
-#
-# data = Data(cloud)
+#               placement_dist='uniform')
+
+cloud = Cloud(num_leafs=48,
+              num_hosts_per_leaf=48,
+              max_vms_per_host=20,
+              num_tenants=100,
+              min_vms_per_tenant=10,
+              max_vms_per_tenant=100,
+              vm_dist='expon',
+              num_groups=1000,
+              min_group_size=5,
+              group_size_dist='uniform',
+              placement_dist='uniform',
+              num_bitmaps=10,
+              generate_bitmaps=True)
+
+data = Data(cloud)
 
 # cloud = Cloud(num_leafs=48,
 #               num_hosts_per_leaf=48,
