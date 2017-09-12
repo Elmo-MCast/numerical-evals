fa26e5c1cba9206ff54a1366dddc26d3e13627f2
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-09-12 00:42:51 -0700

    minor

--- main.sh
+++ main.sh
@@ -7,7 +7,7 @@ for seed in 0 1 2 3 4
 do
     for num_bitmaps in 10 20 30
     do
-        for num_colocate_hosts in 10 20 30 48
+        for num_colocate_hosts in 20 30 48
         do
             python main.py ${TEST_NAME} ${seed} ${num_bitmaps} ${NUM_GROUPS} ${num_colocate_hosts} ./logs
             echo
--- simulation/main.py
+++ simulation/main.py
@@ -5,23 +5,7 @@ from simulation.plot import *
 
 np.random.seed(seed=0)
 
-# cloud = Cloud(num_leafs=48,
-#               num_hosts_per_leaf=48,
-#               max_vms_per_host=20,
-#               num_tenants=100,
-#               min_vms_per_tenant=10,
-#               max_vms_per_tenant=100,
-#               vm_dist='expon',
-#               num_groups=1000,
-#               min_group_size=5,
-#               group_size_dist='uniform',
-#               placement_dist='colocate',
-#               colocate_num_hosts_per_leaf=48,
-#               num_bitmaps=10,
-#               generate_bitmaps=True,
-#               post_process=True)
-
-cloud = Cloud(num_leafs=576,
+cloud = Cloud(num_leafs=1056,
               num_hosts_per_leaf=48,
               max_vms_per_host=20,
               num_tenants=3000,
@@ -32,9 +16,25 @@ cloud = Cloud(num_leafs=576,
               min_group_size=5,
               group_size_dist='uniform',
               placement_dist='colocate',
-              colocate_num_hosts_per_leaf=48,
+              colocate_num_hosts_per_leaf=20,
               num_bitmaps=10,
               generate_bitmaps=True,
               post_process=True)
+
+# cloud = Cloud(num_leafs=576,
+#               num_hosts_per_leaf=48,
+#               max_vms_per_host=20,
+#               num_tenants=3000,
+#               min_vms_per_tenant=10,
+#               max_vms_per_tenant=5000,
+#               vm_dist='expon',
+#               num_groups=100000,
+#               min_group_size=5,
+#               group_size_dist='uniform',
+#               placement_dist='colocate',
+#               colocate_num_hosts_per_leaf=48,
+#               num_bitmaps=10,
+#               generate_bitmaps=True,
+#               post_process=True)
 
 data = Data(cloud)
