a7aa03a37d1363a31724eaa276facdcfccabe459
mshahbaz <mshahbaz@cs.princeton.edu>
2017-11-22 08:20:12 -0500

    minor

--- simulation/algorithms/random_fuzzy_match.py
+++ simulation/algorithms/random_fuzzy_match.py
@@ -7,9 +7,15 @@ def min_k_union(leafs_map, leafs, k, probability):
     min_k_leafs = []
 
     for _ in range(k):
-        leaf = min(leafs, key=lambda l: popcount(leafs_map[l]['bitmap'] | min_k_bitmap))
-        if not random.random() < probability:
-            leaf = min(filter(leaf.__ne__, leafs), key=lambda l: popcount(leafs_map[l]['bitmap'] | min_k_bitmap))
+        temp_leafs = leafs[:]
+        while True:
+            if temp_leafs:
+                leaf = min(temp_leafs, key=lambda l: popcount(leafs_map[l]['bitmap'] | min_k_bitmap))
+                if random.random() < probability:
+                    break
+                temp_leafs.remove(leaf)
+            else:
+                break
         leafs.remove(leaf)
         min_k_bitmap |= leafs_map[leaf]['bitmap']
         min_k_leafs += [leaf]
