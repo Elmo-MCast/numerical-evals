d6bd7a57a2c1a767a9ef3fd5f546026db075180e
Muhammad Shahbaz <mshahbaz@cs.princeton.edu>
2018-01-06 12:34:30 -0500

    minor

--- fab_dynamic_with_data.sh
+++ fab_dynamic_with_data.sh
@@ -2,15 +2,16 @@
 
 DATA_FILE_PREFIX="/mnt/sdb1/baseerat/numerical-evals/12-24-2017/output-1M/optimizer.*_leafs"
 
-declare -i i=1
+#declare -i i=1
 for file in ${DATA_FILE_PREFIX}
 do
     fab -f fab_dynamic_with_data.py run_with_args:${file} &
+    wait
 
-    n=$((i%2))
-    if [ ${n} -eq 0 ]
-    then
-        wait
-    fi
-    i=$((i+1))
+#    n=$((i%2))
+#    if [ ${n} -eq 0 ]
+#    then
+#        wait
+#    fi
+#    i=$((i+1))
 done
