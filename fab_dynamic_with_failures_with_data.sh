bf7564d162b6341f1b1ddec6c975a5e728d22803
Muhammad Shahbaz <mshahbaz@cs.princeton.edu>
2018-05-28 02:49:50 -0400

    minor

--- fab_dynamic_with_failures_with_data.sh
+++ fab_dynamic_with_failures_with_data.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-DATA_FILE_PREFIX="/mnt/sdb1/baseerat/numerical-evals/1-25-2018/output-1M/optimizer.*_leafs"
+DATA_FILE_PREFIX="/mnt/sdb1/baseerat/numerical-evals/5-28-2018/output-1M/optimizer.*_leafs"
 
 #declare -i i=1
 for file in ${DATA_FILE_PREFIX}
 do
-    fab -f fab_dynamic_with_failures_with_data.py run_with_args:"spine"${file} &
-    fab -f fab_dynamic_with_failures_with_data.py run_with_args:"core"${file} &
+    fab -f fab_dynamic_with_failures_with_data.py run_with_args:"spine",${file} &
+    fab -f fab_dynamic_with_failures_with_data.py run_with_args:"core",${file} &
     wait
 
 #    n=$((i%2))
